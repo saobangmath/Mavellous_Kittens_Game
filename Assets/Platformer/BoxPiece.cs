a45aaea50e6ab325276d07fa9adfef241c483eae
siaii <terence.andre@yahoo.com>
2020-03-10 22:15:41 +0700

    Gameplay part
    Current state of my unity project, haven't clean up yet

---
+++ Assets/Platformer/BoxPiece.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPiece : MonoBehaviour
+{
+    [SerializeField] private Vector2 forceDirection;
+
+    private Rigidbody2D rb2d;
+
+    [SerializeField] private float forceMag;
+    [SerializeField] private int torque;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(forceDirection*forceMag);
+        rb2d.AddTorque(torque);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Turn-Based/Scripts/TurnController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour
+{
+
+    private int currentTurn = 0;
+
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject enemy;
+
+    private EnemyScript _enemyScript;
+    private PlayerScript _playerScript;
+
+    private bool attacking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyScript = enemy.GetComponent<EnemyScript>();
+        _playerScript = player.GetComponent<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //++currentTurn;
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            _enemyScript.StartCoroutine("runAttack");
+        }
+
+    }
+
+    public void EnemyAttack()
+    {
+        if (!attacking)
+        {
+            attacking = true;
+            StartCoroutine(delayedEnemyAttack());
+        }
+    }
+
+    public void PlayerAttack()
+    {
+        if (!attacking)
+        {
+            attacking = true;
+            StartCoroutine(delayedPlayerAttack());
+
+        }
+    }
+
+    IEnumerator delayedEnemyAttack()
+    {
+        _enemyScript.StartCoroutine("runAttack");   
+        yield return new WaitForSeconds(2.1f);
+        attacking = false;
+    }
+
+    IEnumerator delayedPlayerAttack()
+    {
+        _playerScript.StartCoroutine("runAttack");
+        yield return new WaitForSeconds(3.2f);
+        attacking = false;
+    }
+}
