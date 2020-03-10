75e70d5630466cf123a0d6c8d114fb21a07c8e9d
siaii <terence.andre@yahoo.com>
2020-03-10 22:21:19 +0700

    Revert "Gameplay part"
    This reverts commit 057401f8bd7ab86ec240f77a0c78a5f43be8a21d.

---
+++ Assets/Turn-Based/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Animator _anim;
+
+    [SerializeField] private HP_Bar bar;
+
+    [SerializeField]
+    private GameObject enemy;
+    [SerializeField] private static int maxHP = 10;
+    private int _hp = maxHP;
+
+    private EnemyScript _enemyScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _enemyScript = enemy.GetComponent<EnemyScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator runAttack()
+    {
+        _anim.SetTrigger("attack");
+        yield return new WaitForSeconds(1.5f);
+        _enemyScript.DecreaseHP();
+    }
+
+    public void DecreaseHP()
+    {
+        _anim.SetTrigger("hit");
+        bar.StartCoroutine(bar.UpdateBar((float)_hp / (float)maxHP, (float)(_hp - 1) / (float)maxHP));
+        _hp -= 1;        
+    }
+}
---
+++ Assets/Turn-Based/Scripts/QnA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QnA
+{
+    private String question;
+    private String[] ansChoice = new String[4];
+    private int correctAns;
+
+    //The Question
+    public string Question
+    {
+        get { return question; }
+        set { question = value; }
+    }
+
+    //Array of answer choices (4)
+    public int CorrectAnswer
+    {
+        get { return correctAns;}
+        set { correctAns = value; }
+    }
+
+    //The correct choice (1 for a, 2 for b, 3 for c, 4 for d)
+    public String[] AnswerChoice
+    {
+        get { return ansChoice; }
+        set { ansChoice = value; }
+    }
+}
