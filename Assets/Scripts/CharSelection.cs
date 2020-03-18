b5d93a32182246c823d04c24fce843750caf5b4b
ashroff2 <61981906+ashroff2@users.noreply.github.com>
2020-03-18 16:12:56 +0800

    Custom Level Unity Scenes

---
+++ Assets/Scripts/CharSelection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharSelection : MonoBehaviour
+{
+  private int selectedEnemyIndex;
+  private Color desiredColor;
+
+  [Header ("Enemy List")]
+  [SerializeField] private List<EnemySelectObject> enemyList = new List<EnemySelectObject>();
+
+
+  [Header("UI References")]
+  //[SerializeField] private TextMeshProGUI charName;
+  [SerializeField] private Image characterSplash;
+  [SerializeField] private Image backgroundColor;
+
+
+  private void Start()
+  {
+    UpdateCharacterSelectionUI();
+  }
+
+  public void LeftArrow()
+  {
+    selectedEnemyIndex--;
+    if (selectedEnemyIndex < 0)
+    {
+      selectedEnemyIndex = enemyList.Count - 1;
+    }
+
+    UpdateCharacterSelectionUI();
+  }
+
+    public void RightArrow()
+  {
+    selectedEnemyIndex++;
+    if (selectedEnemyIndex == enemyList.Count)
+    {
+      selectedEnemyIndex = 0;
+    }
+
+    UpdateCharacterSelectionUI();
+  }
+
+  private void UpdateCharacterSelectionUI()
+  {
+      //Splash, Name, Desired Color
+      characterSplash.sprite = enemyList[selectedEnemyIndex].splash;
+      desiredColor = enemyList[selectedEnemyIndex].EnemyBGColor;
+
+  }
+
+  [System.Serializable]
+  public class EnemySelectObject
+  {
+      public Sprite splash;
+      public Color EnemyBGColor;
+  }
+}
+
+
---
+++ Assets/Scripts/GetQuestions.cs
@@ -0,0 +1,35 @@
+﻿// using Proytecto26;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class player : MonoBehaviour
+// {
+// //  public Dropdown myDropdown; // Make sure to assign this
+// //  private List<string> data;
+ 
+// //  void Start()
+// //  {
+// //       data = LoadDataFromDBOrJSON(); // Or whatever way you will read your data in
+ 
+// //       myDropdown.options.Clear();
+// //       foreach(string str in data)
+// //       {
+// //          myDropdown.options.Add(new Dropdown.OptionData(str));
+// //      }
+// //  }
+
+//  private void Rest()
+//  {
+//      RestClient.get(url: "https://unity-test-8da93.firebaseio.com/questions/5BRH0aE7nbdmCWhVicSb/.json").Then(response =>
+//      {
+
+//         String questionString = response.text;
+//         questionData = JsonUtility.FromJson<Question>(questionString);
+
+
+
+
+//      });
+//  }
+// }
