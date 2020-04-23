8e697ac6f0a7e17d8b6e64f5efd73fdb9ce560a6
Aldo Halim <aldohalim28@gmail.com>
2020-04-23 16:12:18 +0800

    Documentation for MusicObjectScript

--- Assets/Scripts/MusicObjectScript.cs
+++ Assets/Scripts/MusicObjectScript.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+/// <summary>
+/// Handles the playing of music in the game.
+/// </summary>
 [RequireComponent(typeof(AudioSource))]
 public class MusicObjectScript : MonoBehaviour
 {
     private AudioSource _audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +17,9 @@ public class MusicObjectScript : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
     }
 
+    /// <summary>
+    /// Toggles in-game music (on / off).
+    /// </summary>
     public void ToggleMusic()
     {
         _audioSource.mute = !_audioSource.mute;
