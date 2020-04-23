bc9873a67f0cb24add6d804861d3689ed01dd6c4
YingTing04 <47131808+YingTing04@users.noreply.github.com>
2020-04-23 15:55:57 +0800

    Update FireballScript documentation

--- Assets/Scripts/Weapon Script/FireballScript.cs
+++ Assets/Scripts/Weapon Script/FireballScript.cs
@@ -3,15 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class provides the animation for the Fireball thrown by the player.
+/// </summary>
 public class FireballScript : MonoBehaviour
 {
     private Transform _currPos;
+    
+    /// <summary>
+    /// In the Start() method, the current position of the fireball is initialised.
+    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
         _currPos = transform;
     }
-
+    
+    /// <summary>
+    /// This method controls the motion of the fireball.
+    /// </summary>
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +30,9 @@ public class FireballScript : MonoBehaviour
         transform.position = _nextPos;
     }
 
+    /// <summary>
+    /// This method destroys the fireball when it has hit the enemy.
+    /// </summary>
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
