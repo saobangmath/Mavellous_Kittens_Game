9c3ca288d8329cafe675fdcd328f32edabfc4a63
YingTing04 <47131808+YingTing04@users.noreply.github.com>
2020-04-23 15:57:53 +0800

    Update LightningScript documentation

--- Assets/Scripts/Weapon Script/LightningScript.cs
+++ Assets/Scripts/Weapon Script/LightningScript.cs
@@ -3,16 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class provides the animation for the Lightning thrown by the player.
+/// </summary>
 public class LightningScript : MonoBehaviour
 {
     private Transform _currPos;
 
+    /// <summary>
+    /// In the Start() method, the current position of the lightning is initialised.
+    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
         _currPos = transform;
     }
 
+    /// <summary>
+    /// This method controls the motion of the lightning.
+    /// </summary>
     // Update is called once per frame
     void Update()
     {
@@ -21,6 +30,9 @@ public class LightningScript : MonoBehaviour
         transform.position = _nextPos;
     }
 
+    /// <summary>
+    /// This method destroys the lightning when it has hit the enemy.
+    /// </summary>
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
